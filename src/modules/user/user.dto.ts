6720bcb21c733eafbfc88413ed4fde6f46291e38
Viplav jha <viplavjha1993@gmail.com>
2023-12-02 18:35:54 +0530

    user types

---
+++ src/modules/user/user.dto.ts
@@ -0,0 +1,18 @@
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+  export class User {
+    @Field(() => ID,{nullable:false})
+    id: string;
+
+   @Field(() => ID,{nullable:false})
+    username: string;
+
+    @Field(() => ID,{nullable:false})
+    email: string;
+
+  
+    password: string;
+
+}
+
